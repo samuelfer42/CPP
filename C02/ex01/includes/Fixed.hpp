18c6ea2771c09744e1b37c272d897d9485c67ffa
Samuel FERNANDEZ <safernan@e2r13p4.clusters.42paris.fr>
2022-05-14 04:58:25 +0200

    save

--- C02/ex01/includes/Fixed.hpp
+++ C02/ex01/includes/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: safernan <safernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 23:38:24 by safernan          #+#    #+#             */
-/*   Updated: 2022/05/14 03:39:07 by safernan         ###   ########.fr       */
+/*   Updated: 2022/05/14 03:40:57 by safernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,16 @@ class Fixed {
 
 	public:
 		Fixed (void);
+		Fixed (const int entier_value);
+		Fixed (const float float_value);
 		Fixed(const Fixed &cpy);
 		~Fixed (void);
 
 		Fixed 	&operator= (const Fixed &value);
 		int		getRawBits(void) const;
 		void 	setRawBits(int const raw);
+		float	toFLoat (void) const;
+		int		toInt(void) const;
 	
 	private:
 		int					_nfix;
---
+++ C02/ex01/src/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: safernan <safernan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/13 23:38:27 by safernan          #+#    #+#             */
+/*   Updated: 2022/05/14 03:42:15 by safernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Fixed.hpp"
+
+int main( void ) {
+	
+	Fixed 		a;
+	Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
+	
+	a = Fixed( 1234.4321f );
+	
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+	return 0;
+}
