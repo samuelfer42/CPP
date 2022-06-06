05fa5ab8b67ebf278185fd106c61cdd5d7642183
Samuel FERNANDEZ <safernan@e2r13p2.clusters.42paris.fr>
2022-06-06 05:47:40 +0200

    add ex00

---
+++ C08/ex00/includes/easyfind.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: safernan <safernan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/06 05:10:53 by safernan          #+#    #+#             */
+/*   Updated: 2022/06/06 05:41:25 by safernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <algorithm>
+# include <iostream>
+# include <vector>
+
+
+template<typename T>
+typename T::iterator	 easyfind(T &containers, int value)
+{
+	typename T::iterator it;
+	
+	it = std::find(containers.begin(), containers.end(), value);
+	
+	if (it == containers.end())
+		throw std::logic_error("Not found");
+	return(it);
+}
+
+#endif
---
+++ C08/ex00/src/main.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: safernan <safernan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/06 05:10:56 by safernan          #+#    #+#             */
+/*   Updated: 2022/06/06 05:46:58 by safernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/easyfind.hpp"
+
+int		main()
+{
+	std::vector<int>cont_tab(4);
+	std::vector<int>::iterator itr;
+	std::vector<int>::iterator it;
+	
+	cont_tab[0] = 22;
+	cont_tab[1] = 18;
+	cont_tab[2] = 7;
+	cont_tab[3] = 8;
+	int 	find = 8;
+
+
+	std::cout << "Container = { ";
+	for(it = cont_tab.begin(); it!=cont_tab.end(); ++it)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << "}" << std::endl;
+
+	try
+	{
+		itr = easyfind(cont_tab, find);
+		std::cout << "Find ok -> " << (*itr) << std::endl;
+	}
+	catch(std::exception const &e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+}
